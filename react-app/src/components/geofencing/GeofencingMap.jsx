bba70ff130b176776108c10db533f6c2be09c533
Fleet Developer <developer@fleet-dev.com>
2025-04-20 13:29:46 -0400

    Fix useGoogleMapsApi import in GeofencingMap component

--- react-app/src/components/geofencing/GeofencingMap.jsx
+++ react-app/src/components/geofencing/GeofencingMap.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { useGoogleMapsApi } from '../../hooks/useGoogleMapsApi';
+import useGoogleMapsApi from '../../hooks/useGoogleMapsApi';
 
 /**
  * GeofencingMap Component
